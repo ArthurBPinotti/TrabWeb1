02513c7b1948a69f369c879fe5bfa18f1eafe65c
luizklitzke1 <luiz.klitzke1@gmail.com>
2022-04-27 23:37:16 -0300

    Organizacao das pastas e download do css e js do boostrap pra acesso local

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function setaCookie() {
+    var userName = document.getElementById('editNome').value;
+    var userSenha = document.getElementById('editSenha').value;
+
+    if (userName == "" || userSenha == "")
+    {
+        alert("O nome de usuário e a senha devem ser informados!")
+
+        return;
+    }
+
+    document.cookie = "username="+userName; 
+    document.cookie = "password="+userSenha; 
+    console.log(document.cookie);
+    window.location.href = "index.html";
+}
+
+
+function loginCheck() {
+    if (document.cookie != '') {
+        var userName = document.cookie.split(';')[0].split('=')[1];
+        document.getElementById('imgUser').style.display = 'block';
+        document.getElementById('userName').innerHTML = userName;
+    } 
+    else {
+        document.getElementById('imgUser').style.display = 'hidden';
+        document.getElementById('userName').innerHTML = "Usuário não autenticado";
+    }
+}
+
+function customPage(){
+    var element = document.getElementsByTagName('body')[0];
+    element.classList.add('classCustom');
+}
+
+function normalPage(){
+    
+    var element = document.getElementsByTagName('body')[0];
+    element.classList.remove('classCustom');
+}
